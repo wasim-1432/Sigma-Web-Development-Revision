e058f200cbd124fb689ec88e8f5ece286839bdd2
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 04:04:03 +0530

    Hello.

---
+++ JS32/script.js
@@ -0,0 +1,16 @@
+let marks=80;
+if(marks>=33)
+{
+    if(marks>=80)
+    {
+        console.log("O");
+    }
+    else
+    {
+        console.log("A");
+    }
+}
+else
+{
+    console.log("Better luck next time.");
+}
