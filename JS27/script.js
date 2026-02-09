72f1bf033032cba7dac0fd6fb4bf9649dc175339
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-09 21:57:38 +0530

    Hello.

---
+++ JS27/script.js
@@ -0,0 +1,15 @@
+let age=23;
+if(age>=18)
+{
+    console.log("You can vote.");
+}
+if(age<18)
+{
+    console.log("You can not Vote.");
+}
+
+let firstName="Wasim";
+if(firstName=="Wasim")
+{
+    console.log(`Welcome ${firstName}`);
+}
