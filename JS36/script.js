c045130cd0130019152f2afee9d02987edf9b64d
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 04:51:37 +0530

    Hello.

---
+++ JS36/script.js
@@ -0,0 +1,13 @@
+let color="red";
+switch(color)
+{
+    case "red":
+        console.log("Stop!");
+        break;
+    case "yellow":
+        console.log("Ready to move");
+    case "green":
+        console.log("Go");
+    default:
+        console("Broken signal light!");
+}
