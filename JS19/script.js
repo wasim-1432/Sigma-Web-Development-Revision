7c0788812308219d296c7f8e5ca4e099cb87b120
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-09 20:36:42 +0530

    Hello.

---
+++ JS19/script.js
@@ -0,0 +1,16 @@
+let name1="Mohd Wasim";
+console.log(name1.length);
+
+let firstName="Mohd";
+console.log(firstName[0]);
+
+let lastName="Wasim";
+console.log(lastName[lastName.length-1]);
+
+let s="apnacollege"+123;
+console.log(s);
+
+let s1=" ";
+let s2="";
+console.log(s1.length);
+console.log(s2.length);
