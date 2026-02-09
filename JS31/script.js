731c953ba71a9983f41cba5f9210cfdd58cefaa4
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 03:57:11 +0530

    Hello.

---
+++ JS31/script.js
@@ -0,0 +1,17 @@
+let size="XL";
+if(size==="XL")
+{
+    console.log(`Rs. ${250}`);
+}
+else if(size==="L")
+{
+    console.log(`Rs. ${200}`);
+}
+else if(size==="M")
+{
+    console.log(`Rs. ${100}`);
+}
+else
+{
+    console.log(`Rs. ${50}`);
+}
