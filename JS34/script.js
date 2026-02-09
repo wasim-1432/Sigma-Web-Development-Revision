55e864362a47677a1e37557b342309d9d55fb34e
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 04:32:18 +0530

    Hell.

---
+++ JS34/script.js
@@ -0,0 +1,19 @@
+let s="ansdfehsh";
+if(s[0]==='a' && s.length>3)
+{
+    console.log("Good string");
+}
+else
+{
+    console.log("Could not good string");
+}
+
+let num=12;
+if(num%3===0 && (num+1===15 || num-1==11))
+{
+    console.log("Safe");
+}
+else
+{
+    console.log("Not Safe");
+}
