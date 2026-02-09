b9c3a33fbe30bdfdf977f7d722120c16047b0a95
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-09 22:18:52 +0530

    Hello.

---
+++ JS29/script.js
@@ -0,0 +1,27 @@
+let age=23;
+if(age>=18)
+{
+    console.log("You can Vote.");
+}
+else if(age<18)
+{
+    console.log("You can not vote.");
+}
+
+let marks=75;
+if(marks>80)
+{
+    console.log("A+");
+}
+else if(marks>60 && marks<=80)
+{
+    console.log("A");
+}
+else if(marks>33 && marks<=60)
+{
+    console.log("B");
+}
+else if(marks<=33)
+{
+    console.log("Fail");
+}
