95d8971cfaaec302b21ad4ba344db7ff43671414
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-09 21:03:25 +0530

    Hello.

---
+++ JS22/scrpt.js
@@ -0,0 +1,5 @@
+let pencilPrice=3;
+let erasorPrice=5;
+console.log("The total price is :",(pencilPrice+erasorPrice)," Rupees.");
+let output=`The total price is : ${pencilPrice+erasorPrice} Rupees.`;
+console.log(output);
