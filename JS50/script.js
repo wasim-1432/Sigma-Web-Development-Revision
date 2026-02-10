717ec67ddfaf302291d7414fc7aa3ecd1152568a
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 14:12:37 +0530

    Hello.

---
+++ JS50/script.js
@@ -0,0 +1,16 @@
+let msg="help!";
+let s1=msg.trim().toUpperCase();
+console.log(s1);
+
+let s2="ApnaCollege";
+let s3=s2.slice(4,9);
+console.log(s3);
+
+let i=s2.indexOf("na");
+console.log(i);
+
+let s4=s2.replace("Apna","Our");
+console.log(s4);
+
+let s5=s2.slice(4).replace('l','t');
+console.log(s5);
