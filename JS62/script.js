45b5bb14d5b4ffa0f14a29fa0961d77b3a0312f4
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 16:47:52 +0530

    Hello.

---
+++ JS62/script.js
@@ -0,0 +1,8 @@
+let arr=["january","july","march","august"];
+arr.splice(0,1);
+arr.splice(1,0,"june");
+console.log(arr);
+
+let arr1=["c","c++","html","javascript","python","java","c#","sql"];
+arr1.reverse();
+console.log(arr1.indexOf("javascript"));
