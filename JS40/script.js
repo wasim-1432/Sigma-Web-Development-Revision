9fc5f7645459b23e8ee07b4ca3a4576aca561528
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-10 10:15:28 +0530

    Hello.

---
+++ JS40/script.js
@@ -0,0 +1,31 @@
+// let num=119;
+// if(num%10==0)
+// {
+//     console.log("Good Number");
+// }
+// else
+// {
+//     console.log("Bad Number");
+// }
+
+// let name=prompt("Enter your name");
+// let age=prompt("Enter your age");
+// alert(`${name} is ${age} years old.`)
+
+let day= 4;
+switch(day)
+{
+    case 1:
+        console.log(`January,February,March`);
+        break;
+    case 2:
+        console.log(`April,May,June`);
+        break;
+    case 3:
+        console.log(`July,August,September`);
+        break;
+    case 4:
+        console.log(`October,November,December`);
+    default:
+        console.log("Invalid Input!");
+}
