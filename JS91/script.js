07006f94ebf0025dbd2d23312010cfc9cf670ee7
wasim-1432 <wasim.ahmad0661@gmail.com>
2026-02-12 02:09:46 +0530

    Hello.

---
+++ JS91/script.js
@@ -0,0 +1,17 @@
+const arr=[
+    {
+        name:"Aman",
+        grade:"A+",
+        city:"Delhi"
+    },
+    {
+        name:"Sharadha",
+        grade:"B+",
+        city:"Pune"
+    },
+    {
+        name:"Karan",
+        grade:"C+",
+        city:"Hyderabad"
+    }
+]
